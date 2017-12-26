c33d7f53ccda43280335d741504dc06a1485901a
WangSmallHui <wzxwhd@126.com>
2017-12-26 17:36:03 +0800

    ｎｅｗ

---
+++ 31.h
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    /*
+     * @param nums: The integer array you should partition
+     * @param k: An integer
+     * @return: The index after partition
+     */
+    int partitionArray(vector<int> &nums, int k) {
+        // write your code here
+        if(nums.size() < 1)return 0;
+        return partition(nums, k, 0, nums.size()) + 1;
+    }
+    int partition(vector<int> &nums, int k, int l, int r){
+        // 1 三路排序
+        // int lt = l - 1, gt = r;
+        // int i = l;
+        // while(i < gt){
+        //     if(nums[i] == k){
+        //         i++;
+        //     }
+        //     else if(nums[i] > k){
+        //         swap(nums[i], nums[--gt]);    
+        //     }
+        //     else{ // nums[i] < k
+        //         swap(nums[i], nums[++lt]);
+        //         i++;
+        //     }
+        // }
+        // return lt;
+        //　2 对于本题，2路排序就可以
+        int lt = l, gt = r-1;
+        while(true){
+            while(lt < r && nums[lt] < k) lt++;
+            while(gt >= l && nums[gt] >= k) gt--;
+            if(lt >= gt) break;
+            swap(nums[lt++], nums[gt--]);
+        }
+        return gt;
+    }
+};
---
+++ 464.h
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    /*
+     * @param A: an integer array
+     * @return: 
+     */
+    void sortIntegers2(vector<int> &A) {
+        // write your code here
+        mergesort(A, 0, A.size());
+    }
+    //[l,r)
+    void mergesort(vector<int> &A, int l, int r){
+        if(l >= r -1) return;
+        int mid = l + (r - l) / 2;
+        mergesort(A, l, mid);
+        mergesort(A, mid,r);
+        if(A[mid - 1] < A[mid]) return;
+        merge(A,l,mid,r);
+    }
+    //[l, mid) and [mid,r)
+    void merge(vector<int> &A, int l, int mid, int r){
+        int aux[r - l];
+        for(int i = 0; i < r - l; i++)
+            aux[i] = A[i + l];
+        int i = l; // the index of vector aux
+        int j = l, k = mid;
+        while( j < mid || k < r){
+            if(j >= mid){
+                A[i] = aux[k-l];
+                k++;
+            }
+            else if(k >= r){
+                A[i] = aux[j-l];
+                j++;
+            }
+            else if(aux[j-l] < aux[k-l]){
+                A[i] = aux[j-l];
+                j++;
+            }
+            else{ // aux[j] >= aux[k]
+                A[i] = aux[k-l];
+                k++;
+            }
+            i++;
+        }
+    }
+};
