a595614e2be2faecbf53ae77cea5843606493160
WangHui <wzxwhd@126.com>
2018-01-21 14:57:08 +0800

    fix bugs

--- 166.h
+++ 166.h
@@ -23,16 +23,29 @@ public:
         // write your code here
         if(head == nullptr) return head;
         
-        ListNode *dummyhead = head;
-        int length = 0;
-        while(dummyhead != nullptr){
-            length++;
-            dummyhead = dummyhead->next;
-        }
+        // 1
+        // ListNode *dummyhead = head;
+        // int length = 0;
+        // while(dummyhead != nullptr){
+        //     length++;
+        //     dummyhead = dummyhead->next;
+        // }
         
-        for(int i = 0; i < length - n; i++){
-            head = head->next;
+        // for(int i = 0; i < length - n; i++){
+        //     head = head->next;
+        // }
+        // return head;
+        
+        // 2 两根指针
+        int k = 0;
+        ListNode *fast = head, *slow = head;
+        while(k++ < n){
+            fast = fast->next;
         }
-        return head;
+        while(fast != nullptr){
+            fast = fast->next;
+            slow = slow->next;
+        }
+        return slow;
     }
 };
