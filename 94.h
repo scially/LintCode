1c4d34393ea025fae0785024dcf0cb4b18a046df
WangHui <wzxwhd@126.com>
2018-01-26 20:54:14 +0800

    fix bugs

--- 94.h
+++ 94.h
@@ -19,30 +19,27 @@ public:
      * @return: An integer
      */
     struct ResultType{
-        int singlepath; //某个节点左右两个子树最大的路径，不包括root节点
-        int maxpath;
-        ResultType(int s, int m): singlepath(s), maxpath(m){}
+        int root2any; //某个节点左右两个子树最大的路径，不包括root节点
+        int any2any;
+        ResultType(int s, int m): root2any(s), any2any(m){}
     };
     
     int maxPathSum(TreeNode * root) {
         // write your code here
        ResultType r = any2Any(root);
-       return r.maxpath;
+       return r.any2any;
     }
     
     ResultType any2Any(TreeNode *root){
-        if(root == nullptr) return ResultType(0, INT_MIN);
+        if(root == nullptr) return ResultType(INT_MIN, INT_MIN);
         
         ResultType l = any2Any(root->left) ;
         ResultType r = any2Any(root->right);
         
-        // root2Any
-        // 对于这一行代码来说，求的是从root到任一节点最大的路径
-        // 当某个节点值为负时，我们从这儿断开，存为0，这样当回到父节点时，最大值就是父节点
-        int singlepath = max(max(l.singlepath, r.singlepath) + root->val, 0);
-        int maxpath = max(l.singlepath + r.singlepath + root->val, 
-                          max(l.maxpath, r.maxpath));
+        int root2any = max(max(l.root2any, r.root2any),0 ) + root->val;
+        int any2any = max(max(l.any2any, r.any2any),  //左右子树分别的最大路径
+                          max(l.root2any, 0) + max(r.root2any, 0) + root->val);
     
-        return ResultType(singlepath, maxpath);
+        return ResultType(root2any, any2any);
     }
 };
